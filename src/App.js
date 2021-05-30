9a51b775e61bf0c8cfba28004f7600240ab4f435
beinhorn1990 <71242936+beinhorn1990@users.noreply.github.com>
2021-05-30 18:37:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [game, setGame] = useState({ board: [] });
+
+  async function newEasyGameClick() {
+    const response = await fetch(
+      "https://minesweeper-api.herokuapp.com/games?difficulty=0",
+      {
+        method: "POST",
+      }
+    );
+
+    const gameReponse = await response.json();
+    setGame(gameReponse);
+  }
+
+  async function clickLeftie(x, y) {
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games/${game.id}/check?row=${y}&col=${x}`,
+      { method: "POST" }
+    );
+    const gameReponse = await response.json();
+    setGame(gameReponse);
+  }
+
+  async function clickRightie(x, y) {
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games/${game.id}/flag?row=${y}&col=${x}`,
+      { method: "POST" }
+    );
+    const gameReponse = await response.json();
+    setGame(gameReponse);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Minesweeper</h1>
+      <button onClick={newEasyGameClick}>New Easy Game</button>
+      <button>New Medium Game</button>
+      <button>New Hard Game </button>
+      <div className="game">
+        {game.board.map((row, y) => (
+          <div key={y} className="row">
+            {row.map((col, x) => (
+              <div
+                onClick={() => clickLeftie(x, y)}
+                key={x}
+                className="column"
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  clickRightie(x, y);
+                }}
+              >
+                {col}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {game.state === "lost" && <h2>Sorry, try again</h2>}
+      {game.state === "won" && <h2>Hey, you won and are really smart</h2>}
+    </>
   );
 }
 
 export default App;
+
